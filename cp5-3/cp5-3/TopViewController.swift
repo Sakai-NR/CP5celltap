33196348010ef4018cc0d4ecb05915f398bd642e
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-10 22:33:21 +0900

    00003

---
+++ cp5-3/cp5-3/TopViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TopViewController.swift
+//  cp5-3
+//
+//  Created by 酒井典昭 on 2019/09/10.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    @IBOutlet weak var mainview: TopMainView!
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+        mainview.delegate = self
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController:TopMainViewDelegate {
+    func selectedRow(indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            print("1")
+        default:
+            print("2-7")
+        }
+    }
+    
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
