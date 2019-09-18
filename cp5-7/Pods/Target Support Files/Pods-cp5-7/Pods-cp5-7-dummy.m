0fe81b8e45b0b46c3d280a84417283bc4f1d92f6
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-18 12:28:12 +0900

    00007

---
+++ cp5-7/Pods/Target Support Files/Pods-cp5-7/Pods-cp5-7-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cp5_7 : NSObject
+@end
+@implementation PodsDummy_Pods_cp5_7
+@end
---
+++ cp5-7/cp5-7/TopViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TopViewController.swift
+//  cp5-7
+//
+//  Created by 酒井典昭 on 2019/09/18.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    @IBOutlet weak var mainView: TopMainView!
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+        mainView.delegate = self
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController:TopMainViewDelegate {
+    func selectedRow(indexPath: IndexPath) {
+        let secondViewController = SecondViewController()
+        transitionViewController(from: self, to: secondViewController)
+    }
+    
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
